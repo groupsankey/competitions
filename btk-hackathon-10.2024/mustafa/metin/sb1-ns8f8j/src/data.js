aca6ef5f57dab1ac8a965044aefa36ce88bf660e
Mustafa <mustafaemrekapan2905@gmail.com>
2024-10-28 09:23:58 +0300

    whiteboard changes

---
+++ btk-hackathon-10.2024/mustafa/metin/sb1-ns8f8j/src/data.js
@@ -0,0 +1,19 @@
+export const classes = [
+  {
+    id: 'class1',
+    name: 'Math Class',
+    boards: [
+      { id: 'board1', name: 'Algebra' },
+      { id: 'board2', name: 'Geometry' },
+    ],
+  },
+  {
+    id: 'class2',
+    name: 'Science Class',
+    boards: [
+      { id: 'board3', name: 'Physics' },
+      { id: 'board4', name: 'Chemistry' },
+    ],
+  },
+];
+
